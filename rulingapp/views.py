60ef372be1d7491c3ddc8ba35f3b197f5b0db960
Nikita <nbajrach@mail.sfsu.edu>
2019-10-21 18:09:52 -0700

    Designed Landing Page

--- rulingapp/views.py
+++ rulingapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def landing(request):
-    name = "Nikita Bajracharya"
-    return render(request, 'index.html',{'name':name})
+def home(request):
+    #name = "Nikita Bajracharya"
+    return render(request, 'index.html',{})
